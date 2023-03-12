eca9686ec15c64b109114a5ebd695820206e4ede
Emma Paris <eparis@atlassian.com>
2023-03-12 19:55:19 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+
+        int initialAccount = 500;
+        int amountDeposit = 1200;
+        int bonus;
+
+        if (amountDeposit > 1000) {
+            bonus = amountDeposit / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println((initialAccount + amountDeposit + bonus) + " Сумма на счету");
     }
+
 }
